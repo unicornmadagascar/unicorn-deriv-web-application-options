b496ab0e09600686d138f6690325094c22af20c5
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-05 21:19:34 +0300

    Update web version

--- app.js
+++ app.js
@@ -373,11 +373,11 @@ document.addEventListener("DOMContentLoaded", () => {
                   // Filtrer les contrats SELL (Boom/Crash → MULTDOWN)
                   const sellContracts = contracts.filter(c => c.contract_type === "MULTDOWN" && c.symbol === currentSymbol);
 
-                  console.log(`🔴 ${sellContracts.length} contrats SELL trouvés.`);
+                  //console.log(`🔴 ${sellContracts.length} contrats SELL trouvés.`);
 
                   // Fermer chaque contrat SELL
                   sellContracts.forEach(c => {
-                      console.log(`🛑 Fermeture du contrat ${c.contract_id} (${c.symbol})`);
+                      //console.log(`🛑 Fermeture du contrat ${c.contract_id} (${c.symbol})`);
                       wsAutomation.send(JSON.stringify({ sell: c.contract_id, price: 0 }));
                   });
 
@@ -393,11 +393,11 @@ document.addEventListener("DOMContentLoaded", () => {
                   // Filtrer les contrats BUY (ex: CALL, RISE, ou basés sur ton type)
                   const buyContracts = contracts.filter(c => c.contract_type === "MULTUP" && c.symbol === currentSymbol);
 
-                  console.log(`🟢 ${buyContracts.length} contrats BUY trouvés`);
+                  //console.log(`🟢 ${buyContracts.length} contrats BUY trouvés`);
 
                   // Fermer chaque contrat
                   buyContracts.forEach(c => {
-                     console.log(`🟢 Fermeture du contrat ${c.contract_id} (${c.symbol})`);
+                     //console.log(`🟢 Fermeture du contrat ${c.contract_id} (${c.symbol})`);
                      wsAutomation.send(JSON.stringify({ sell: c.contract_id, price: 0 }));
                   });
 
