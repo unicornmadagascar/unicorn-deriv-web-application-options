5bb824006e9144ea14d7a0bef63b270ea3c1feef
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-13 17:53:47 +0300

    Update web version

--- app.js
+++ app.js
@@ -278,8 +278,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (currentInterval === "1 tick" && (currentChartType !== "candlestick" || currentChartType !== "hollow" || currentChartType !== "ohlc"))
       {
-       //wspl.send(JSON.stringify({ forget_all: ticks }));    
-       wspl.send(JSON.stringify({ ticks: symbol, subscribe: 1 }));        
+       wspl.send(JSON.stringify({ forget_all: "ticks" }));    
+       wspl.send(JSON.stringify({ ticks: symbol, subscribe: 1 }));          
       }        
       else if (currentInterval !== "1 tick")    
       {
