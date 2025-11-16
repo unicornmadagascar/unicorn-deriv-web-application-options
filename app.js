cd0aef324a935828e6e61f0876af2d631cd6f111
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-16 12:05:19 +0300

    Update web version

--- app.js
+++ app.js
@@ -347,7 +347,7 @@ document.addEventListener("DOMContentLoaded", () => {
                       style: "candles", 
                       subscribe: 1 // C'est la clé pour les mises à jour en temps réel !
                };
-
+    
                console.log('Envoi de la demande de données OHLC:', ohlcRequest);
                wspl__ct.send(JSON.stringify(ohlcRequest));
                console.log('Demande de données OHLC envoyée pour le symbole:', symbol);   
