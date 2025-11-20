4b531476315078c9369d92095a5554b2d8d393c7
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-20 22:18:14 +0300

    Update web version

--- app.js
+++ app.js
@@ -342,7 +342,7 @@ document.addEventListener("DOMContentLoaded", () => {
           };
 
           if (!bar) return;  
-
+   
           const last = candles[candles.length - 1];
 
           if (!last || last.time !== openTime) {
