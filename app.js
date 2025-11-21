74cba0d1e5f75cbd371f10687c35632faf5c4b32
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-21 20:20:04 +0300

    Update web version

--- app.js
+++ app.js
@@ -155,8 +155,8 @@ document.addEventListener("DOMContentLoaded", () => {
     { symbol: "frxEURUSD", name: "EURUSD" },
     { symbol: "frxGBPUSD", name: "GBPUSD" },   
     { symbol: "frxUSDJPY", name: "USDJPY" },
-    { symbol: "OTC_SPC", name: "US 500" },
-    { symbol: "OTC_DJI", name: "WS 30" }  
+    { symbol: "R_50", name: "VIX 50" },
+    { symbol: "R_75", name: "VIX 75" }  
   ];      
   const fmt = n => Number(n).toFixed(2);
   const safe = v => (typeof v === "number" && !isNaN(v)) ? v : 0;
