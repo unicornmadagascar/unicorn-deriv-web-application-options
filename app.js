f03e8bb8ae60b1b5135c672081ddc8dc6088bb33
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-22 22:46:01 +0300

    Update web version

--- app.js
+++ app.js
@@ -1182,7 +1182,7 @@ document.addEventListener("DOMContentLoaded", () => {
     wsContracts = new WebSocket(WS_URL);
     wsContracts.onopen=()=>{ wsContracts.send(JSON.stringify({ authorize: TOKEN })); };
 
-    if(data.authorize && wsContracts && wsContracts.readyState===WebSocket.OPEN){
+    if(wsContracts && wsContracts.readyState===WebSocket.OPEN){
        const payload = {
         buy: 1,
         price: stake.toFixed(2),
