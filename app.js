3db4442a2303a71af4f7eec51c9b3fe7e564e6ae
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-11-22 20:52:25 +0300

    Update web version

--- app.js
+++ app.js
@@ -477,7 +477,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (wspl && (wspl.readyState === WebSocket.CLOSED || wspl.readyState === WebSocket.CLOSING)) {
       wspl = new WebSocket(WS_URL);
-      wspl.onopen=()=>{ wspl.send(JSON.stringify({ authorize: TOKEN })); };  
+      wspl.onopen=()=>{ wspl.send(JSON.stringify({ authorize: TOKEN })); };    
     }
   
     wspl.onmessage = (evt) => {
