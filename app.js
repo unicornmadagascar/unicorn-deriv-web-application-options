31fd167d15e57a1169e83bdcbfae5638f3355f29
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-12-01 21:12:05 +0300

    Updates web version

--- app.js
+++ app.js
@@ -431,7 +431,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     if (ws && (ws.readyState === WebSocket.CLOSED || ws.readyState === WebSocket.CLOSING)) {
-      ws = new WebSocket(WS_URL);
+      ws = new WebSocket(WS_URL);   
       ws.onopen=()=>{ ws.send(JSON.stringify({ authorize: TOKEN })); };      
     }
 
