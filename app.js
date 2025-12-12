3c6dcf242bcded964e05992eb7409386c10b7cbb
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-12-12 22:46:56 +0300

    Updates web version

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Éléments UI
   const openCashierBtn = document.getElementById("openCashierBtn");
   const closePopupBtn = document.getElementById("closeCashierBtn");
-  const cashierModal = document.getElementById("cashierModal");
+  const cashierModal = document.getElementById("cashierModal");  
   const emailInput = document.getElementById("emailInput");
   const codeInput = document.getElementById("codeInput");
   const sendEmailBtn = document.getElementById("sendEmailBtn");
