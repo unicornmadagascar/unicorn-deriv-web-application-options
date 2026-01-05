8074212d66d5a57b9e750cd0a76cc3a2ab49b6a0
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-05 21:28:08 +0300

    Updates web version

--- app.js
+++ app.js
@@ -1519,7 +1519,7 @@ closeAll.onclick=()=>{
   /* ================== UTILS ================== */
   const tp2xy = (t,p)=>({
      x: chart.timeScale().timeToCoordinate(t),
-     y: candleSeries.priceToCoordinate(p)
+     y: currentSeries.priceToCoordinate(p)
   });
 
   /* ================== CANVAS OVERLAY ================== */
