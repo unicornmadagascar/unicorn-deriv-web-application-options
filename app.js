49848f076b380ddbbedeb604465b30c06b79a38e
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-08 10:16:37 +0300

    Updates web version

--- app.js
+++ app.js
@@ -370,7 +370,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function connect(symbol, currentInterval, currentChartType) {
 
-    if (ws) { ws.close(); ws = null; }
+    //if (ws) { ws.close(); ws = null; }
 
     if (!symbol) return;
 
