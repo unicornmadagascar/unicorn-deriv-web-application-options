214030547ec0657a6322e9bdd3cda78a9e027c96
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-16 15:54:39 +0300

    Updates web version

--- app.js
+++ app.js
@@ -4459,7 +4459,7 @@ document.addEventListener("DOMContentLoaded", () => {
     drawingObjects.forEach(obj => {
       const x1 = chart.timeScale().timeToCoordinate(obj.p1.time);  
       const y1 = currentSeries.priceToCoordinate(obj.p1.price);
-      const x2 = chart.timeScale().timeToCoordinate(obj.p2.time);
+      const x2 = chart.timeScale().timeToCoordinate(obj.p2.time);  
       const y2 = currentSeries.priceToCoordinate(obj.p2.price);  
 
       // Si on clique près d'une extrémité
