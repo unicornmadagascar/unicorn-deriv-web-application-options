03ee41ab26a00c90b354c4fb43dea70a67871a0f
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-16 14:42:37 +0300

    Updates web version

--- app.js
+++ app.js
@@ -4429,7 +4429,7 @@ document.addEventListener("DOMContentLoaded", () => {
     drawingObjects.forEach(obj => {
       const x1 = chart.timeScale().timeToCoordinate(obj.p1.time);
       const y1 = currentSeries.priceToCoordinate(obj.p1.price);
-      const x2 = chart.timeScale().timeToCoordinate(obj.p2.time);
+      const x2 = chart.timeScale().timeToCoordinate(obj.p2.time);  
       const y2 = currentSeries.priceToCoordinate(obj.p2.price);
 
       if (Math.hypot(x - x1, y - y1) < 15 || Math.hypot(x - x2, y - y2) < 15) {
