411f96f7dec0d499d1bbadef8ad312b3ad3de768
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-17 15:00:45 +0300

    Updates web version

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let dragOffset = null;
   let showDrawings = true; // État de visibilité global
   let fiboObj = null;      // Outil Fibonacci
-  let vpLookback = 700;    // Période ajustable pour le Volume Profile
+  let vpLookback = 700;    // Période ajustable pour le Volume Profile  
   let isFiboLocked = false; // État du verrouillage
   // ================== x ==================
 
