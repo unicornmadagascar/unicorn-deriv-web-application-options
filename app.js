f2cfd853b4c7274bc656274cd83538897e9e6d0a
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-19 21:21:15 +0300

    Updates web version

--- app.js
+++ app.js
@@ -388,7 +388,7 @@ document.addEventListener("DOMContentLoaded", () => {
         selectedSymbol = symbol;
         currentSymbol = selectedSymbol;
         console.log(`Render : ${selectedSymbol}`);
-        // E. Activer le bouton de validation
+        // E. Activer le bouton de validation  
         if (validateBtn) validateBtn.disabled = false;
       };
 
