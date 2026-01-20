d89011fb097d9e7dafec4a452e9d3c43cb83b9e8
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-20 14:55:08 +0300

    Updates web version

--- app.js
+++ app.js
@@ -659,7 +659,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }  
 
     const lastClose = candles[candles.length - 1].close;
-    const baseRatio = (currentATR / lastClose) * 100;
+    const baseRatio = (currentATR / lastClose) * 100;  
   
     // --- RÉGLAGE : Sensibilité réduite ---
     // On passe de 1500 à 1000 pour calmer l'amplitude
