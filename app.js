0191eac362e7ebd728ae16718393611109a5353a
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-20 20:09:28 +0300

    Updates web version

--- app.js
+++ app.js
@@ -565,7 +565,7 @@ document.addEventListener("DOMContentLoaded", () => {
       emaArray.push((data[i] - emaArray[i - 1]) * k + emaArray[i - 1]);
     }
   
-    console.log(`emaArray : ${emaArray}`);     
+    console.log(`emaArray : ${emaArray}`);      
     
     return emaArray;  
   }
