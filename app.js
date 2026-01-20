f07f207b64294fb0c9269b63fa0a38f498074411
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-20 17:27:51 +0300

    Updates web version

--- app.js
+++ app.js
@@ -736,7 +736,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('txt-vol-val').innerText = Math.round(smoothedVol) + "%";
     document.getElementById('txt-vol-label').innerText = label;  
   } 
-
+  
   /**
    * FONCTION PRINCIPALE À APPELER
    * @param {Array} candles - Données reçues de l'API Deriv
