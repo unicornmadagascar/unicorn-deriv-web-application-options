ffff0358f1b6e8fc61cc00bd1712ad596a1899b2
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-22 18:36:24 +0300

    Updates web version

--- app.js
+++ app.js
@@ -2911,7 +2911,7 @@ document.addEventListener("DOMContentLoaded", () => {
               const canvas = document.getElementById('Trendoverlay__');
               if (canvas) {
                 const ctx = canvas.getContext('2d');
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.clearRect(0, 0, canvas.width, canvas.height);  
               }
             }
           }
