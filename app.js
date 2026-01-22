76d9270b622fa31579e6a2a4cdd726d33264e82e
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-22 18:37:49 +0300

    Updates web version

--- app.js
+++ app.js
@@ -2916,7 +2916,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
           }  
         } catch (e) {  
-          console.error("Erreur moteur Sniper/Bollinger:", e);
+          console.error("Erreur moteur Sniper/Bollinger:", e);  
         }
       }
     });
