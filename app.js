554d705922533d80edd3d687528fa54697bc6202
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-26 23:23:11 +0300

    Updates web version

--- app.js
+++ app.js
@@ -4953,7 +4953,7 @@ document.addEventListener("DOMContentLoaded", () => {
         ctx.font = "bold 12px Arial";
         ctx.fillText(`Ratio R/R: ${rr}`, x1 + 5, Math.min(yTP, yEntry) - 10);
       }
-      ctx.restore();  
+      ctx.restore();    
     }
   }
 
