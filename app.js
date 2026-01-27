3cb8de8628713d67fc20c5f184101bdf7441cbb3
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2026-01-27 14:22:50 +0300

    Updates web version

--- app.js
+++ app.js
@@ -3984,7 +3984,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     ws4update.onerror = (err) => {
-      ws4update.close();
+      ws4update.close(); 
       ws4update = null;
       setTimeout(initPortfolioStream, 2000);            
     };
